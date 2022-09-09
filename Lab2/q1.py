e3f306cfaa059eb14c33ea350af10983ba96870b
DiMarzio Bian <326361233@qq.com>
2022-09-10 07:15:59 +0800

    update

--- Lab2/q1.py
+++ Lab2/q1.py
@@ -11,7 +11,13 @@ def add_noise(img, level):
     return img_new
 
 
-def filter_noise(img, level):
+def filter_median(img, level):
+    bins = np.arange(1, level) / level * 256 - 1
+    img_new = np.digitize(img, bins)
+    return img_new
+
+
+def filter_gaussian(img, level):
     bins = np.arange(1, level) / level * 256 - 1
     img_new = np.digitize(img, bins)
     return img_new
@@ -19,35 +25,35 @@ def filter_noise(img, level):
 
 def main():
     img_l = cv2.imread('Lena.bmp', flags=0)  # flags = 0 to read grayscale images
-    img_m = cv2.imread('Mandrill.bmp', flags=0)
-    img_p = cv2.imread('Peppers.bmp', flags=0)
+    img_f = cv2.imread('flower.tif', flags=0)
 
-    i = 0
-    map_level = {0: 2, 1: 4, 2: 8, 3: 16, 4: 64}
-    level = map_level[i]
+    img_ln = add_noise(img_l)
+    img_fn = add_noise(img_f)
 
+    img_lnm = filter_median(img_ln)
+    img_lng = filter_gaussian(img_ln)
 
-    img_l = cv2.imread('Lena.bmp', flags=0)  # flags = 0 to read grayscale images
-    img_m = cv2.imread('Mandrill.bmp', flags=0)
-    img_p = cv2.imread('Peppers.bmp', flags=0)
+    img_fnm = filter_median(img_fn)
+    img_fng = filter_gaussian(img_fn)
 
-    print('Start histogram transferring...')
-    img_l_new, hist_l, hist_l_new = histTransfer(img_l, img_m)
-    img_m_new, hist_m, hist_m_new = histTransfer(img_m, img_p)
-    img_p_new, hist_p, hist_p_new = histTransfer(img_p, img_l)
-
-    print(f'Drawing histogram transferred images...')
     # lena to mandrill
     plt.figure(0)
-    fig, axs = plt.subplots(3, 2)
+    fig, axs = plt.subplots(2, 2)
     fig.set_size_inches(10, 10)
-    fig.suptitle(f'Transferred "Lena.bmp" from histogram of "Mandrill.bmp".')
-    axs[0, 0].imshow(img_l, cmap='Greys_r')
-    axs[0, 1].plot(hist_l)
-    axs[1, 0].imshow(img_m, cmap='Greys_r')
-    axs[1, 1].plot(hist_m)
-    axs[2, 0].imshow(img_l_new, cmap='Greys_r')
-    axs[2, 1].plot(hist_l_new)
+    axs[0, 0].plot(img_l)
+    axs[0, 1].plot(img_ln)
+    axs[1, 0].plot(img_lnm)
+    axs[1, 1].plot(img_fnm)
+    plt.show()
+
+    # lena to mandrill
+    plt.figure(1)
+    fig, axs = plt.subplots(2, 2)
+    fig.set_size_inches(10, 10)
+    axs[0, 0].plot(img_f)
+    axs[0, 1].plot(img_fn)
+    axs[1, 0].plot(img_lng)
+    axs[1, 1].plot(img_fng)
     plt.show()
 
 
