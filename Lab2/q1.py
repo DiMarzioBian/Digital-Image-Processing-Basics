8396f8c7b8a0fbdde2c208a75e5a7014d498a9da
DiMarzio Bian <326361233@qq.com>
2022-09-10 08:25:09 +0800

    update

--- Lab2/q1.py
+++ Lab2/q1.py
@@ -6,7 +6,7 @@ import keyboard
 
 
 def add_noise(img, mean=0, sigma=1, snr_sp=0.8):
-    n_g = np.random.normal(mean, sigma, img.shape)  # gaussian noise
+    w_g = np.random.normal(mean, sigma, img.shape)  # gaussian noise
     n_sp = np.random.choice([0, -1, 1], size=img.shape, replace=True, p=[snr_sp, (1-snr_sp)/2, (1-snr_sp)/2])
     img += n_g
     img[n_sp == -1] = 0
@@ -44,28 +44,38 @@ def main():
 
     img_lnm = filter_median(img_ln)
     img_lng = filter_gaussian(img_ln)
-
     img_fnm = filter_median(img_fn)
     img_fng = filter_gaussian(img_fn)
 
-    # lena to mandrill
+    mse_lnm = np.mean(np.power((img_fnm-img_f), 2))
+    mse_lng = np.mean(np.power((img_fng-img_f), 2))
+    mse_fnm = np.mean(np.power((img_fnm-img_f), 2))
+    mse_fng = np.mean(np.power((img_fng-img_f), 2))
+
     plt.figure(0)
     fig, axs = plt.subplots(2, 2)
     fig.set_size_inches(10, 10)
-    axs[0, 0].plot(img_l)
-    axs[0, 1].plot(img_ln)
-    axs[1, 0].plot(img_lnm)
-    axs[1, 1].plot(img_fnm)
+    axs[0, 0].imshow(img_l, cmap='Greys_r')
+    axs[0, 0].set_title(f'Original image: lena.tif')
+    axs[0, 1].imshow(img_ln, cmap='Greys_r')
+    axs[0, 1].set_title(f'Added gaussian and salt & pepper noises')
+    axs[1, 0].imshow(img_lnm, cmap='Greys_r')
+    axs[1, 0].set_title(f'Median filter, MSE = {mse_lnm}')
+    axs[1, 1].imshow(img_lng, cmap='Greys_r')
+    axs[1, 1].set_title(f'Gaussian filter, MSE = {mse_lng}')
     plt.show()
 
-    # lena to mandrill
     plt.figure(1)
     fig, axs = plt.subplots(2, 2)
     fig.set_size_inches(10, 10)
-    axs[0, 0].plot(img_f)
-    axs[0, 1].plot(img_fn)
-    axs[1, 0].plot(img_lng)
-    axs[1, 1].plot(img_fng)
+    axs[0, 0].imshow(img_f, cmap='Greys_r')
+    axs[0, 0].set_title(f'Original image: lena.tif')
+    axs[0, 1].imshow(img_fn, cmap='Greys_r')
+    axs[0, 1].set_title(f'Added gaussian and salt & pepper noises')
+    axs[1, 0].imshow(img_fnm, cmap='Greys_r')
+    axs[1, 0].set_title(f'Median filter, MSE = {mse_fnm}')
+    axs[1, 1].imshow(img_fng, cmap='Greys_r')
+    axs[1, 1].set_title(f'Gaussian filter, MSE = {mse_fng}')
     plt.show()
 
 
