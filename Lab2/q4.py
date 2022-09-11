ade922379f9c1179bcab2215cb3b5ab206b5d79c
DiMarzio Bian <326361233@qq.com>
2022-09-11 10:58:01 +0800

    update

--- Lab2/q4.py
+++ Lab2/q4.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+from numpy.fft import fft2, fftshift, ifft2, ifftshift
 import matplotlib.pyplot as plt
 
 """
@@ -9,8 +10,8 @@ import matplotlib.pyplot as plt
 """
 
 
-def filter_band_pass_oval(f_img, D0_LP, D0_HP, n):
-    H, W = f_img.shape
+def filter_band_pass_ellipse(img_ffts, D0_LP, D0_HP, n):
+    H, W = img_ffts.shape
     H_filter = np.zeros((H, W), dtype=np.float)
 
     for h in range(H):
@@ -19,35 +20,35 @@ def filter_band_pass_oval(f_img, D0_LP, D0_HP, n):
             H_filter[h, w] += 1 / (1 + (D0_LP / D) ** (2 * n))
             H_filter[h, w] += 1 / (1 + (D / D0_HP) ** (2 * n))
 
-    ff_img = f_img * H_filter
-    return np.abs(np.fft.ifft2(np.fft.ifftshift(ff_img))), ff_img, H_filter
+    img_ffts *= H_filter
+    return np.abs(ifft2(ifftshift(img_ffts))), img_ffts, H_filter
 
 
 def main():
     img = cv2.imread('pepper_corrupt.tif', flags=0)  # flags = 0 to read grayscale images
-    f_img = np.fft.fft2(img)
-    fs_img = np.fft.fftshift(f_img)
+    img_fft = fft2(img)
+    img_ffts = fftshift(img_fft)
 
     D0_LP = 18
     D0_HP = 10
     n = 100
-    img_r_2, f_img_r_2, h_f_2 = filter_band_pass_oval(fs_img, D0_LP=D0_LP, D0_HP=D0_HP, n=n)
+    img_r, img_fft_r, h_f = filter_band_pass_ellipse(img_ffts, D0_LP=D0_LP, D0_HP=D0_HP, n=n)
 
     plt.figure(0)
     fig, axs = plt.subplots(2, 3)
     fig.set_size_inches(15, 10)
     axs[0, 0].imshow(img, cmap='Greys_r')
-    axs[0, 0].set_title(f'Original image: lena.tif')
-    axs[0, 1].imshow(np.log1p(np.abs(f_img)), cmap='Greys_r')
+    axs[0, 0].set_title(f'Original image: pepper_corrupt.tif')
+    axs[0, 1].imshow(np.log1p(np.abs(img_fft)), cmap='Greys_r')
     axs[0, 1].set_title(f'images in freq domain')
-    axs[0, 2].imshow(np.log1p(np.abs(fs_img)), cmap='Greys_r')
+    axs[0, 2].imshow(np.log1p(np.abs(img_ffts)), cmap='Greys_r')
     axs[0, 2].set_title(f'images in freq domain shifted')
 
-    axs[1, 0].imshow(h_f_2, cmap='Greys_r')
+    axs[1, 0].imshow(h_f, cmap='Greys_r')
     axs[1, 0].set_title(f'Band-pass filter | ({D0_LP}, {D0_HP}, n={n})')
-    axs[1, 1].imshow(np.log1p(np.abs(f_img_r_2)), cmap='Greys_r')
+    axs[1, 1].imshow(np.log1p(np.abs(img_fft_r)), cmap='Greys_r')
     axs[1, 1].set_title(f'Restored image in freq domain shifted')
-    axs[1, 2].imshow(img_r_2, cmap='Greys_r')
+    axs[1, 2].imshow(img_r, cmap='Greys_r')
     axs[1, 2].set_title(f'Restored image')
 
     plt.show()
--- Lab2/q5.py
+++ Lab2/q5.py
@@ -1,64 +1,67 @@
 import cv2
 import numpy as np
+from numpy.fft import fft2, fftshift, ifft2, ifftshift
 import matplotlib.pyplot as plt
 
+"""
+    H[u, v] = 1 / (1 + (D0 / D) ** (2 * n))
+    RuntimeWarning: divide by zero encountered in double_scalars
+    Since D is zero, (D0 / D) will be infinity and the final value will be set to 0
+"""
 
-def histTransfer(img_1, img_2):
-    hist_1 = cv2.calcHist([img_1], channels=[0], mask=None, histSize=[256], ranges=[0.0, 255.0])
-    hist_2 = cv2.calcHist([img_2], channels=[0], mask=None, histSize=[256], ranges=[0.0, 255.0])
-    img_1_new = cv2.equalizeHist(img_1, hist_2)
-    hist_1_new = cv2.calcHist([img_2], channels=[0], mask=None, histSize=[256], ranges=[0.0, 255.0])
-    return img_1_new, hist_1, hist_1_new
+
+def get_psf(shape, angle, dist):
+    assert shape[0] == shape[1]
+    center = shape[0] / 2
+    sinVal = np.sin(angle * np.pi / 180)
+    cosVal = np.cos(angle * np.pi / 180)
+    psf = np.zeros(shape)
+    for i in range(dist):
+        x_offset, y_offset = np.round(sinVal * i), np.round(cosVal * i)
+        psf[int(center - x_offset), int(center + y_offset)] = 1
+
+    return psf / psf.sum()
+
+
+def filter_wiener(img_fft, psf_fft, snr=0.01):
+    psf_fft_conj = np.conj(psf_fft) / (np.abs(psf_fft) ** 2 + 1 / snr)
+    return np.abs(fftshift(ifft2(img_fft * psf_fft_conj)))
+
+
+def filter_inverse(img_fft, psf_fft):
+    return np.abs(fftshift(ifft2(img_fft / psf_fft)))
 
 
 def main():
-    img_l = cv2.imread('Lena.bmp', flags=0)  # flags = 0 to read grayscale images
-    img_m = cv2.imread('Mandrill.bmp', flags=0)
-    img_p = cv2.imread('Peppers.bmp', flags=0)
+    angle, dist, snr = 180, 3, 0.99
+    eps = 1e-5
 
-    print('Start histogram transferring...')
-    img_l_new, hist_l, hist_l_new = histTransfer(img_l, img_m)
-    img_m_new, hist_m, hist_m_new = histTransfer(img_m, img_p)
-    img_p_new, hist_p, hist_p_new = histTransfer(img_p, img_l)
+    img = cv2.imread('coins_blurred.tif', flags=0)  # flags = 0 to read grayscale images
+    img_fft = fft2(img)
 
-    print(f'Drawing histogram transferred images...')
-    # lena to mandrill
+    psf = get_psf(img_fft.shape, angle, dist)
+    psf_fft = fft2(psf) + eps
+
+    img_i = filter_inverse(img_fft, psf_fft)
+    img_w = filter_wiener(img_fft, psf_fft, snr=snr)
+
     plt.figure(0)
-    fig, axs = plt.subplots(3, 2)
-    fig.set_size_inches(10, 10)
-    fig.suptitle(f'Transferred "Lena.bmp" from histogram of "Mandrill.bmp".')
-    axs[0, 0].imshow(img_l, cmap='Greys_r')
-    axs[0, 1].plot(hist_l)
-    axs[1, 0].imshow(img_m, cmap='Greys_r')
-    axs[1, 1].plot(hist_m)
-    axs[2, 0].imshow(img_l_new, cmap='Greys_r')
-    axs[2, 1].plot(hist_l_new)
-    plt.show()
+    fig, axs = plt.subplots(2, 3)
+    fig.set_size_inches(15, 10)
+    axs[0, 0].imshow(img, cmap='Greys_r')
+    axs[0, 0].set_title(f'Original image: coins_blurred.tif')
+    axs[0, 1].imshow(np.log1p(np.abs(img_fft)), cmap='Greys_r')
+    axs[0, 1].set_title(f'images in freq domain')
+    axs[0, 2].imshow(psf, cmap='Greys_r')
+    axs[0, 2].set_title(f'PSF | angle={angle}, dist={dist}')
 
-    # mandrill to peppers
-    plt.figure(1)
-    fig, axs = plt.subplots(3, 2)
-    fig.set_size_inches(10, 10)
-    fig.suptitle(f'Transferred "Mandrill.bmp" from histogram of "Peppers.bmp".')
-    axs[0, 0].imshow(img_m, cmap='Greys_r')
-    axs[0, 1].plot(hist_m)
-    axs[1, 0].imshow(img_p, cmap='Greys_r')
-    axs[1, 1].plot(hist_p)
-    axs[2, 0].imshow(img_m_new, cmap='Greys_r')
-    axs[2, 1].plot(hist_m_new)
-    plt.show()
+    axs[1, 0].imshow(np.log1p(np.abs(psf_fft)), cmap='Greys_r')
+    axs[1, 0].set_title(f'PSF in freq domain | eps={eps}')
+    axs[1, 1].imshow(img_i, cmap='Greys_r')
+    axs[1, 1].set_title(f'Inverse filtered image')
+    axs[1, 2].imshow(img_w, cmap='Greys_r')
+    axs[1, 2].set_title(f'Wiener filtered image | snr={snr}')
 
-    # peppers to lena
-    plt.figure(2)
-    fig, axs = plt.subplots(3, 2)
-    fig.set_size_inches(10, 10)
-    fig.suptitle(f'Transferred "Peppers.bmp" from histogram of "Lena.bmp".')
-    axs[0, 0].imshow(img_p, cmap='Greys_r')
-    axs[0, 1].plot(hist_p)
-    axs[1, 0].imshow(img_l, cmap='Greys_r')
-    axs[1, 1].plot(hist_l)
-    axs[2, 0].imshow(img_p_new, cmap='Greys_r')
-    axs[2, 1].plot(hist_p_new)
     plt.show()
 
 
